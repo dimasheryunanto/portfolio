bb8c35f8fdc18f42dd1274d4b373ffd5ebdeb49a
Riandi Kartiko <callmediko@live.com>
2020-03-02 23:44:19 +0700

    Updates

---
+++ precache-manifest.2db46f26077420c6d22466b1a4867864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1194edbefe6244f5ad05f0c2bce0e72f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "42f8bf1e6be6b8789a13",
+    "url": "/portfolio/static/css/main.7bcc1bc2.chunk.css"
+  },
+  {
+    "revision": "a99e4a1fb7f4270c3b21",
+    "url": "/portfolio/static/js/2.4e92ce17.chunk.js"
+  },
+  {
+    "revision": "42f8bf1e6be6b8789a13",
+    "url": "/portfolio/static/js/main.f4a87089.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
